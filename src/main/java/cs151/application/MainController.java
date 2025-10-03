3dc6cf4d41e54b8009f23977d0ee1d9bdfdbfc33
Trxppy7 <bhurani26@gmail.com>
2025-10-03 14:49:24 -0700

    Update MainController.java

--- src/main/java/cs151/application/MainController.java
+++ src/main/java/cs151/application/MainController.java
@@ -13,7 +13,7 @@ import java.io.IOException;
 public class MainController {
     @FXML
     protected void onProgramDefine(ActionEvent event) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml")); // Update hello-view.fxml to language define page file
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Main-page.fxml")); // Updated to Main-page.fxml to language define page file
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         Stage stage = new Stage();
         stage.setTitle("Programming Language Define");
