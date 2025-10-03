396c9df5225a476b80270e975d8b23b9b1dffd39
Sohi10 <143568321+Sohi10@users.noreply.github.com>
2025-10-03 13:04:03 -0700

    Update MainController.java

--- src/main/java/cs151/application/MainController.java
+++ src/main/java/cs151/application/MainController.java
@@ -1,7 +1,14 @@
 package cs151.application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainController {
     @FXML
