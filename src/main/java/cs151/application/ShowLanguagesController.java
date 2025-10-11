33d7d38ade0e4ae551756a345764a0c29e97c7af
Trxppy7 <bhurani26@gmail.com>
2025-10-11 09:36:27 -0700

    ShowLanguagesController.java Done

--- src/main/java/cs151/application/ShowLanguagesController.java
+++ src/main/java/cs151/application/ShowLanguagesController.java
@@ -9,27 +9,23 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class ShowLanguagesController {
 
-    // Reference to the TableView in the FXML file
     @FXML
     private TableView<String> languageTable;
 
-    // The column that will show each programming language
     @FXML
     private TableColumn<String, String> languageColumn;
 
     @FXML
     public void initialize() {
-        // Load saved languages from file into memory
         LanguagesData.getInstance().loadLanguagesFromFile();
 
-        // Get the in-memory list of languages
         ObservableList<String> data = LanguagesData.getInstance().getLanguages();
 
-        // Set up the table column to show the actual string values
+        // Creates table column
         languageColumn.setCellValueFactory(cellData ->
                 new SimpleStringProperty(cellData.getValue()));
 
-        // Display the list in the table
+        // Shows the list
         languageTable.setItems(data);
     }
 }
