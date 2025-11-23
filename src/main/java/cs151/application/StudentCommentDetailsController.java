c971190bc43c9695953b28a8e9c4098afd5f647a
v28011git <154228671+v28011git@users.noreply.github.com>
2025-11-22 18:12:20 -0800

    Create StudentCommentDetailsController.java

---
+++ src/main/java/cs151/application/StudentCommentDetailsController.java
@@ -0,0 +1,46 @@
+package cs151.application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import java.time.format.DateTimeFormatter;
+
+// this is the controller for displaying the full comment
+public class StudentCommentDetailsController {
+
+    @FXML private Label date;
+    @FXML private TextArea commentTextArea;
+    private Stage stageVal;
+    private static final DateTimeFormatter FORMAT_DATE = DateTimeFormatter.ofPattern("MM/dd");
+
+    public void setStage(Stage stage) {
+        stageVal = stage;
+    }
+
+    // use to set the values to the page
+    // add comment to the text area
+    public void setComment(StudentReportComment comment) {
+        if (comment == null) return;
+
+        // will format the date
+        if (comment.getDate() != null) {
+            date.setText(comment.getDate().format(FORMAT_DATE));
+        } else {
+            date.setText("");
+        }
+
+        // gets the text of the comment
+        // then adds the comment text to the text ara
+        String commentText = comment.getCommentText();
+        commentTextArea.setText(commentText == null ? "" : commentText);
+    }
+
+    // when closing page
+    @FXML
+    private void onClose() {
+        if (stageVal != null) {
+            stageVal.close();
+        }
+    }
+}
